a3d53adb45ba00d2e2c1b1c0912ecd1b5f653644
Sanitreddy Gudipati <sanitreddy@gmail.com>
2024-06-20 18:31:04 +0530

    Minor modification

--- rds-secrets-manager-Task6/awscli.sh
+++ rds-secrets-manager-Task6/awscli.sh
@@ -1,3 +1,1 @@
 aws secretsmanager create-secret --name myrdscreds --secret-string '{"username":"mydbuser","password":"mysecurepassword"}'
-
-arn:aws:secretsmanager:ap-south-1:654654530894:secret:myrdscreds-CkBGhv
